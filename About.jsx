730311c357c7e1ba4b535b4289d76c18f348eed0
Otii12 <otiendestephen12@gmail.com>
2026-02-08 13:01:03 +0300

    Add About component with project details

---
+++ About.jsx
@@ -0,0 +1,19 @@
+import './About.css'
+
+function About() {
+  return (
+    <div className="about">
+      <h1>About Us</h1>
+      <p>This is a full-stack website built with React and Node.js</p>
+      <h2>Our Stack</h2>
+      <ul>
+        <li>Frontend: React with Vite</li>
+        <li>Backend: Node.js with Express</li>
+        <li>Database: MongoDB</li>
+        <li>Styling: CSS3</li>
+      </ul>
+    </div>
+  )
+}
+
+export default About
